1bdc2376af6cd7271012083ec9642f1eb56df912
Alex-Muirhead <alexandermuirhead@hotmail.com>
2022-09-08 23:05:14 +1000

    Add solutions use fft

---
+++ Week_06/README.md
@@ -0,0 +1,57 @@
+# Solutions
+
+While the ICT session questions required analytical calculations for the
+coefficients of the sine and cosine approximations, we can use the
+[`scipy.fft`](https://docs.scipy.org/doc/scipy/reference/fft.html) library to
+numerically evaluate these coefficients. These solutions are given in
+[fft_solutions.py](./fft_solutions.py), and are similar to the analytical
+solutions for the visualisation.
+
+## Q1 and Q3
+
+Both Q1 and Q3 involve purely sine representations. If the coefficients $a_n$
+are known (from the `fft` module), then we can reconstruct the sample with
+$$
+y(x) = 2\sum_{n=1}^{N} a_{n-1} \sin\left(nx\right).
+$$
+Note that we index the coefficients with $n-1$, since the `fft` module does not
+return a "zeroth" coefficent.
+
+In order to calculate the coefficients, we can call `scipy.fft.dst`. The main
+restriction with this function, is that it assumes the data we give it is on
+the domain $[0, \pi]$. Sometimes, this means scaling the $x$ in our above
+reconstruction.
+
+```python
+from scipy import fft
+
+#                --- Discrete Sine Transform
+amplitudes = fft.dst(data, norm="forward")  # <- Ensures correct scaling
+#                    ---- Assumes data is over [0, pi]
+
+approx = 0
+for n in range(1, N+1):
+    approx += amplitudes[n-1] * np.sin(n*x)
+```
+
+## Q2
+
+A similar approach can be taken to Q2, except that we use `scipy.fft.dct` for
+the discrete cosine transform. The recreation is slightly different too, as we
+have to account for the "zeroth" coefficient being at a zero frequency, and is
+technically counted twice.
+$$
+y(x) = a_{0} + 2\sum_{n=1}^{N} a_{n} \cos\left(nx\right).
+$$
+In Python, this can be done as
+```python
+from scipy import fft
+
+#                --- Discrete Sine Transform
+amplitudes = fft.dct(data, norm="forward")  # <- Ensures correct scaling
+#                    ---- Assumes data is over [0, pi]
+
+approx = amplitudes[0]
+for n in range(1, N+1):
+    approx += amplitudes[n] * np.cos(n*x)
+```
---
+++ Week_06/fft_solutions.py
@@ -0,0 +1,84 @@
+# Standard NumPy and Matplotlib import
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import FFT library
+from scipy import fft
+
+
+def Q1():
+    sample = np.linspace(0, np.pi, num=501)
+    exact  = np.ones_like(sample)
+
+    _, ax = plt.subplots()
+    ax.plot(sample, exact)
+
+    N = [5, 21]
+    amplitudes = fft.dst(exact, norm="forward", type=1)
+
+    domain = np.linspace(-2*np.pi, +2*np.pi, num=501)
+    approx = np.zeros_like(domain)  # Make it the same size
+    for n in range(1, max(N)+1):
+        a_n = 2*amplitudes[n-1]
+        approx += a_n * np.sin(domain*n)
+
+        if n in N:
+            ax.plot(domain, approx, label=f'N={n}')
+
+    plt.show()
+
+
+def Q2():
+    sample = np.linspace(0, np.pi, num=501)
+    exact  = sample**2
+
+    _, ax = plt.subplots()
+    ax.plot(sample, exact)
+
+    N = [5, 21]
+    amplitudes = fft.dct(exact, norm="forward")
+
+    domain = np.linspace(-2*np.pi, +2*np.pi, num=501)
+    approx = amplitudes[0] * np.ones_like(domain)  # Make it the same size
+    for n in range(1, max(N)+1):
+        a_n = 2*amplitudes[n]
+        approx += a_n * np.cos(domain*n)
+
+        if n in N:
+            ax.plot(domain, approx, label=f'N={n}')
+
+    plt.show()
+
+
+def Q3():
+    sample = np.linspace(0, np.pi, num=501)
+    exact  = sample
+
+    _, ax = plt.subplots()
+    ax.plot(sample, exact)
+
+    N = [5, 21]
+    amplitudes = fft.dst(exact, norm="forward", type=1)
+
+    domain = np.linspace(-2*np.pi, +2*np.pi, num=501)
+    approx = np.zeros_like(domain)  # Make it the same size
+    for n in range(1, max(N)+1):
+        a_n = 2*amplitudes[n-1]
+        approx += a_n * np.sin(domain*n)
+
+        if n in N:
+            ax.plot(domain, approx, label=f'N={n}')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    questions = (Q1, Q2, Q3)
+    for question in questions:
+        input(f'Press `Enter` to run {question.__name__} ')
+
+        plt.close('all')        # <- Close all existing figures
+        question()
+        plt.show(block=False)   # <- Allow code execution to continue
+
+    input('Press `Enter` to quit the program.')
