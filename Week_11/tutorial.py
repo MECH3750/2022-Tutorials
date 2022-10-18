427bded1505f10711a8171eb1a9b440393d9a49b
Alex-Muirhead <alexandermuirhead@hotmail.com>
2022-10-18 21:38:57 +1000

    Formatting

--- Week_11/tutorial.py
+++ Week_11/tutorial.py
@@ -57,40 +57,40 @@ M = np.zeros((w*h, w*h))
 for i in range(h):  # Row
     for j in range(w):  # Column
         s = i*w + j
-        
-        # Bottom 
+
+        # Bottom
         if i == 0:
-            M[s,s] = 1
+            M[s, s] = 1
             continue
         # Top
         elif i == h-1:
-            M[s,s] = 1
+            M[s, s] = 1
             continue
-        # Left 
+        # Left
         elif j == 0:
-            M[s,s] = 1
+            M[s, s] = 1
             continue
         # Right
         elif j == w-1:
-            M[s,s] = 1
+            M[s, s] = 1
             continue
-        
+
         # Horizontal derivative (d^2T/dx^2)
         # T_{i,j-1} -2*T_{i,j} + T_{i,j+1}
         # T_{s-1} - 2*T_s + T_{s+1}
-        M[s,s-1] += -1*sigma
-        M[s,s]   += +2*sigma
-        M[s,s+1] += -1*sigma
-        
+        M[s, s-1] += -1*sigma
+        M[s, s] += +2*sigma
+        M[s, s+1] += -1*sigma
+
         # Vertical derivative (d^2T/dy^2)
         # T_{i-1,j} -2*T_{i,j} + T_{i+1,j}
         # T_{s-w} - 2*T_s + T_{s+w}
-        M[s,s-w] += -1*sigma
-        M[s,s]   += +2*sigma
-        M[s,s+w] += -1*sigma
-        
+        M[s, s-w] += -1*sigma
+        M[s, s] += +2*sigma
+        M[s, s+w] += -1*sigma
+
         # One side of time derivative
-        M[s,s] += 1
+        M[s, s] += 1
 
 fig, ax = plt.subplots()
 ax.matshow(M)
@@ -101,7 +101,7 @@ plt.show()
 for _ in range(5):
     T_new = np.linalg.solve(M, T)
     T = T_new
-    
+
 fig, ax = plt.subplots()
 ax.matshow(T.reshape((h, w)), origin='lower')
 plt.show()
